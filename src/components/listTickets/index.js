eb3b9dde9f7abc9f69d2616f5b656cea5f8b3933
Евгений Жиров <girov189916@gmail.com>
2021-02-06 22:43:15 +0300

    1

--- src/components/listTickets/index.js
+++ src/components/listTickets/index.js
@@ -3,7 +3,3 @@ import {ListTickets} from "./ListTickets"
 
 export  default ListTickets
 
-
-import './ListSortTickets.css';
-import {ListSortTickets} from "./ListSortTickets"
-export  default ListSortTickets

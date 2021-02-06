e1b8437678c001b503404b864b11858b6af1be15
Евгений Жиров <girov189916@gmail.com>
2021-02-06 22:40:32 +0300

    1

--- src/components/filterCountStops/FilterCountStops.js
+++ src/components/filterCountStops/FilterCountStops.js
@@ -2,7 +2,6 @@
 import React from "react";
 import {useSelector} from 'react-redux'
 import  ListFilterStops  from "../listFilterStops";
-
 export const FilterCountStops = ()=>{
 
 const loading =   useSelector((state)=>state.initialListTickets.loading)
--- src/components/listTickets/index.js
+++ src/components/listTickets/index.js
@@ -2,3 +2,8 @@ import "./ListTickets.css";
 import {ListTickets} from "./ListTickets"
 
 export  default ListTickets
+
+
+import './ListSortTickets.css';
+import {ListSortTickets} from "./ListSortTickets"
+export  default ListSortTickets
